93ee640da4a74d751098afc40f6ab67d3f97755f
ttasjwi <ttasjwi920@gmail.com>
2022-02-10 10:09:41 +0900

    Initial commit

---
+++ src/main/java/com/ttasjwi/study/HttpRequest/HttpRequestApplication.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.study.HttpRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttasjwi/study/HttpRequest/HttpRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.study.HttpRequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
